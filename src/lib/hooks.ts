b0a91f856d830aa90c4f043e36592eed9e7e941c
N-E-K-0 <samiul.islam94@hotmail.com>
2024-07-30 14:15:24 +0600

    fix: build error

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -150,7 +150,7 @@ export function useLocalStorage<T>(
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
-  return [value, setValue] as const;
+  return [value, setValue];
 }
 
 //-------------------------------------------------------------
